3495addc6618d56084ddbf90ac2533248b8544a8
Florin Bucherer <florin.bucherer@gmail.com>
2022-09-13 15:58:59 +0200

    I added Comments to the Code

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-const fullVociDE =["Hallo", "Tschüss", "Haus", "Tisch"];
-const FullVociEN =["Hello", "Goodby", "House", "Table"];
-let vociDe = ["Hallo", "Tschüss", "Haus", "Tisch"];
+const fullVociDE =["Hallo", "Tschüss", "Haus", "Tisch"]; //Alle Vocis
+const FullVociEN =["Hello", "Goodby", "House", "Table"]; 
+let vociDe = ["Hallo", "Tschüss", "Haus", "Tisch"];//Vocis, die noch gelernt werden müssen
 let vociEn = ["Hello", "Goodby", "House", "Table"];
 let progress = [2, 2, 2, 2]
-let wordIndex;
+let wordIndex; //Variablen für später definieren
 let wordIndexFull;
 let auswahl;
 let ordre;
@@ -12,14 +12,14 @@ let enword;
 let notactive;
 
 //Cookie:
-
+//Cookie erstellen:
 function setCookie(cname, cvalue, exdays) {
     const d = new Date();
     d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
     let expires = "expires="+d.toUTCString();
     document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
   }
-  
+  //Cookie auslesen
   function getCookie(cname) {
     let name = cname + "=";
     let ca = document.cookie.split(';');
@@ -34,7 +34,7 @@ function setCookie(cname, cvalue, exdays) {
     }
     return "";
   }
-  
+  //Vorhandensein eines Cookies überprüfen
   function checkCookie(cname) {
     let cookiename = getCookie(cname);
     if (cookiename != "") {
@@ -44,94 +44,101 @@ function setCookie(cname, cvalue, exdays) {
       }
     }
   
-
-
-function store(ID){
-    setCookie(VociID, ID, 365)
-    return "store"
-}
+//End off Cookie
+//Get a random Integer:
 function getRandomInt(min, max) {
   return Math.floor(Math.random() * (max - min) ) + min;
 }
 
-
+//Generation of everythin:
 function chooseword(){
-    if (progress[wordIndexFull]==0){
-      vociDe.splice(wordIndexFull,1);
-      vociEn.splice(wordIndexFull,1);
+    if (progress[wordIndexFull]==0){ //Wenn der Progress dieses Wortes Null ist:
+      vociDe.splice(wordIndexFull,1); //Wort aus vociDe entfernen
+      vociEn.splice(wordIndexFull,1); //Wort aus vociEn entfernen
     }
-    document.getElementById("right").style.display="none";
-    document.getElementById("wrong").style.display="none";
-    wordIndex = getRandomInt(0,vociDe.length); //Index in VociDe des wort, das abgefragt wird
-    deword = vociDe[wordIndex];
-    enword = vociEn[wordIndex];
+    document.getElementById("right").style.display="none"; //Richtig Banner ausblenden
+    document.getElementById("wrong").style.display="none"; //Falsch Banner ausblenden
+    wordIndex = getRandomInt(0,vociDe.length); //Index in VociDe des wortes, das abgefragt wird
+    deword = vociDe[wordIndex]; //Deutsches Wort
+    enword = vociEn[wordIndex]; //Englische übersetzung
     wordIndexFull = fullVociDE.indexOf(deword); //Index des Wort in fullVociDE
-    document.getElementById("word").textContent=deword;
-    auswahl = [FullVociEN[getRandomInt(0,fullVociDE.length)], FullVociEN[getRandomInt(0,fullVociDE.length)], FullVociEN[getRandomInt(0,fullVociDE.length)]]; //Die drei falschen zur auswahl stehenden Antwort
+    document.getElementById("word").textContent=deword; //Wort auf Webseite schreiben
+    auswahl = [FullVociEN[getRandomInt(0,fullVociDE.length)], FullVociEN[getRandomInt(0,fullVociDE.length)], FullVociEN[getRandomInt(0,fullVociDE.length)]]; //Die drei falschen zur auswahl stehenden Antwortsmöglichkeiten generieren.
     ordre = getRandomInt(1,5);//position der richtigen Antwort
-    notactive = false;
-
-    if (ordre==1){
-     document.getElementById("button1").textContent=enword;
-     document.getElementById("button2").textContent=auswahl[0];
-     document.getElementById("button3").textContent=auswahl[1];
-     document.getElementById("button4").textContent=auswahl[2];
+    notactive = false;//Variable, die verhindert, dass man zweimal Antworten kann -> Hier wird sie auf False gesetzt -> man kann Antwort wählen.
+    if (progress[wordIndexFull]>1){ //Wenn Index grösser als 1-> Multiple Choice
+        document.getElementById("choose").style.display="flex"//choose einblenden
+        document.getElementById("write").style.display="none"//write ausblenden
+        //Buttons beschriften:
+        if (ordre==1){
+        document.getElementById("button1").textContent=enword;
+        document.getElementById("button2").textContent=auswahl[0];
+        document.getElementById("button3").textContent=auswahl[1];
+        document.getElementById("button4").textContent=auswahl[2];
+       }
+   
+       if (ordre==2){
+         document.getElementById("button1").textContent=auswahl[0];
+         document.getElementById("button2").textContent=enword;
+         document.getElementById("button3").textContent=auswahl[1];
+         document.getElementById("button4").textContent=auswahl[2];
+        }
+   
+        if (ordre==3){
+         document.getElementById("button1").textContent=auswahl[0];
+         document.getElementById("button2").textContent=auswahl[1];
+         document.getElementById("button3").textContent=enword;
+         document.getElementById("button4").textContent=auswahl[2];
+        }
+   
+        if (ordre==4){
+         document.getElementById("button1").textContent=auswahl[0];
+         document.getElementById("button2").textContent=auswahl[1];
+         document.getElementById("button3").textContent=auswahl[2];
+         document.getElementById("button4").textContent=enword;
+       }
+      
+    }
+    if (progress[wordIndexFull] == 1){ //Wenn Progress gleich 1 -> Antwort per Tastatureingabe
+      document.getElementById("choose").style.display="none"//choose ausblenden
+      document.getElementById("write").style.display="flex"//write einblenden
     }
 
-    if (ordre==2){
-      document.getElementById("button1").textContent=auswahl[0];
-      document.getElementById("button2").textContent=enword;
-      document.getElementById("button3").textContent=auswahl[1];
-      document.getElementById("button4").textContent=auswahl[2];
-     }
 
-     if (ordre==3){
-      document.getElementById("button1").textContent=auswahl[0];
-      document.getElementById("button2").textContent=auswahl[1];
-      document.getElementById("button3").textContent=enword;
-      document.getElementById("button4").textContent=auswahl[2];
-     }
-
-     if (ordre==4){
-      document.getElementById("button1").textContent=auswahl[0];
-      document.getElementById("button2").textContent=auswahl[1];
-      document.getElementById("button3").textContent=auswahl[2];
-      document.getElementById("button4").textContent=enword;
-    }
     console.log(progress);
     console.log(vociDe);
     console.log(wordIndexFull);
     console.log(fullVociDE);
 }
-
+//Wird ausgeführt, wenn der richtige Button gedrückt wurde.
 function trueanswer(){
-  notactive=true;
-  document.getElementById("right").style.display="flex";
-  progress[wordIndexFull] = progress[wordIndexFull]-1;
-  setTimeout(function() {
+  notactive=true;//Man kann nicht mehr antworten -> keine zweite Antwort möglich
+  document.getElementById("right").style.display="flex";//Richtig Banner wird eingeblendet
+  progress[wordIndexFull] = progress[wordIndexFull]-1;//Progress um 1 gesenkt
+  setTimeout(function() {//warte 800ms befor wieder chooseword() aufgerufen wird
     chooseword();
   }, 800);
 }
-
+//Wenn Falscher Button gedrückt wird:
 function falseanswer(){
-  notactive=true;
-  document.getElementById("wrong").style.display="flex";
-  progress[wordIndexFull] = progress[wordIndexFull]+1;
-  document.getElementById("rightword").textContent=enword;
+  notactive=true;//Man kann nicht mehr antworten -> keine zweite Antwort möglich
+  document.getElementById("wrong").style.display="flex";//Falsch Banner einblenden
+  progress[wordIndexFull] = progress[wordIndexFull]+1;//Progress um 1 erhöhen
+  document.getElementById("rightword").textContent=enword;//richtiges Wort anzeigen
   return
 }
-
+//wird bei klick auf den jeweiligen button ausgeführt:
 function button1(){
-  while (notactive === true){
+  while (notactive === true){//wenn man nicht Antorten kann->funktion beenden
     return
   }
-  if (ordre == 1){
+  if (ordre == 1){//wenn dieser Button die richtige Antwort ist:
     console.log("true");
-    trueanswer();
+    trueanswer();//Führe trueanswer() aus
   }
-  else {
+  else {//ansonsten:
     console.log("false")
-    falseanswer();
+    falseanswer();//führe falseanswer() aus.
   }
   return
 }
